c4e70833332707aed2f004575840c9517c3c7cb1
teeradui <teerada_b@cmu.ac.th>
2026-01-06 12:24:32 +0700

    add navigation & events

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'WebView',
-      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
       home: const MyHomePage(title: 'My WebView'),
     );
   }
@@ -36,8 +38,25 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      //..loadRequest(Uri.parse('https://flutter.dev'));
-      ..loadFlutterAsset('assets/index.html');
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (url) {
+            print("Page Started loading: $url");
+          },
+          onPageFinished: (url) {
+            print("Page Finished loading: $url");
+          },
+          onNavigationRequest: (request) {
+            if (request.url.startsWith("https://flutter.dev/")) {
+              return NavigationDecision.navigate;
+            }
+            print("Blocked navigation to: ${request.url}");
+            return NavigationDecision.prevent;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse("https://flutter.dev/"));
+      //..loadFlutterAsset('assets/index.html');
   }
 
   @override
@@ -45,7 +64,29 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text("WebView Navigation & Events"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () async {
+              if (await _controller.canGoBack()) {
+                _controller.goBack();
+              }
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.arrow_forward),
+            onPressed: () async {
+              if (await _controller.canGoForward()) {
+                _controller.goForward();
+              }
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () => _controller.reload(),
+          ),
+        ],
       ),
       body: WebViewWidget(controller: _controller),
     );
